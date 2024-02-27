e19f225d3f5b484d55d2daca02178570d325aaa8
Gustavo Batista Felix <gustavofelix01@yahoo.com>
2024-02-27 00:02:14 -0300

    acoes ao criar a maquina

--- main.tf
+++ main.tf
@@ -16,7 +16,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
   key_name = "iac-alura"
+  user_data = <<-EOF
+                    #!/bin/bash
+                        cd /home/ubuntu
+                        echo "<h1>Hello World feito com Terraform</h1>" > index.html
+                        nohup busybox httpd -f -p 8080 &
+                EOF
   tags = {
-    Name = "Primeira instancia"
+    Name = "Teste aws"
   }
 }

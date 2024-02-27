f79023487a40a0ef60f43bc61f27a4225756481c
Gustavo Batista Felix <gustavofelix01@yahoo.com>
2024-02-27 01:05:48 -0300

    instalando python com o ansible

--- main.tf
+++ main.tf
@@ -16,13 +16,7 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
   key_name = "iac-alura"
-#   user_data = <<-EOF
-#                     #!/bin/bash
-#                         cd /home/ubuntu
-#                         echo "<h1>Hello World feito com Terraform</h1>" > index.html
-#                         nohup busybox httpd -f -p 8080 &
-#                 EOF
   tags = {
-    Name = "Teste aws"
+    Name = "terraform Ansible Python"
   }
 }
